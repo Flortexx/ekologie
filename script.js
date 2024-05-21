1ece45b24b07b0ca9c43fc6ef7cc7b0a1bda3113
Flortexx <145441012+Flortexx@users.noreply.github.com>
2024-05-21 12:58:41 +0200

    HOTOVO

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMore(section) {
+    switch(section) {
+        case 'solar':
+            alert(' k výrobě elektrické energie nebo tepla. Je jedním z nejčistších a nejdostupnějších zdrojů obnovitelné energie.Solární energie je energie získaná ze slunce pomocí různých technologií, jako jsou fotovoltaické panely nebo solární termální kolektory.');
+            break;
+        case 'wind':
+            alert(', které přeměňují kinetickou energii větru na elektrickou energii. Je to další klíčový zdroj obnovitelné energie.Větrná energie využívá větrné turbíny k přeměně kinetické energie větru na elektrickou energii.');
+            break;
+        case 'recycling':
+            alert('a produkty. Pomáhá snižovat množství odpadu, šetřit přírodní zdroje a snižovat znečištění.Recyklace zahrnuje sběr a zpracování materiálů, které by jinak byly odpadem, aby se mohly opět použít.');
+            break;
+    }
+}
